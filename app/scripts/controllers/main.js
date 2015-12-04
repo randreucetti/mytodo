3d8f19435d1609fb3d496747c0fe3be01f2f2224
randreucetti <ross.andreucetti@gmail.com>
2015-12-04 20:37:03 +0800

    fixed dependencies

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,13 +1,26 @@
 'use strict';
 
 angular.module('mytodoApp')
-  .controller('MainCtrl', function($scope) {
-    $scope.todos = ['Item 1', 'Item 2', 'Item 3'];
+  .controller('MainCtrl', function($scope, localStorageService) {
+
+    // var todosInStore = localStorageService.get('todos');
+
+    $scope.todos =  [];
+    $scope.todo = '';
+
+    $scope.$watch('todos', function() {
+      localStorageService.set('todos', $scope.todos);
+    }, true);
+
     $scope.addTodo = function() {
-      $scope.todos.push($scope.todo);
-      $scope.todo = '';
+      if (!$scope.todos[$scope.todo]) {
+        $scope.todos.push($scope.todo);
+        $scope.todo = '';
+      }
     };
+
     $scope.removeTodo = function(index) {
       $scope.todos.splice(index, 1);
     };
+
   });
